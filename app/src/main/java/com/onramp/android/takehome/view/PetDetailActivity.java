863c316703eecf5d82e593effac0c4afc15dfb2d
meghancodes <meghan.williams6@gmail.com>
2019-03-21 21:30:36 -0700

    Initial design setup

---
+++ app/src/main/java/com/onramp/android/takehome/view/PetDetailActivity.java
@@ -0,0 +1,15 @@
+package com.onramp.android.takehome.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onramp.android.takehome.R;
+
+public class PetDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_detail);
+    }
+}
---
+++ app/src/main/java/com/onramp/android/takehome/view/SearchResultsActivity.java
@@ -0,0 +1,15 @@
+package com.onramp.android.takehome.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onramp.android.takehome.R;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+    }
+}
